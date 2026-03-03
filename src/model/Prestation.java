e60657e3658714dd2eba1982b25a2df049a9ef9d
Shoko <a.breyton.pro@gmail.com>
2026-03-03 13:56:40 +0100

    feat: creater the service class model

---
+++ src/model/Prestation.java
@@ -0,0 +1,39 @@
+package model;
+
+/**
+ * Since I want my github to be fully in english, I will write the comments in
+ * english.
+ * Represents a service sold by the gym (ex: Sauna, Coach...).
+ */
+public class Prestation {
+    private String code;
+    private String label;
+    private double price;
+
+    public Prestation(String code, String label, double price) {
+        this.code = code;
+        this.label = label;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Service{" +
+                "code='" + code + '\'' +
+                ", label='" + label + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
